9c05bb26310b13064d248cb5596da37db389d531
hyunho <melbua@naver.com>
2026-04-07 23:10:51 +0900

    feat:배포용 파일 추가

--- src/test/java/com/neogulss/neogulmap/NeogulMapApplicationTests.java
+++ src/test/java/com/neogulss/neogulmap/NeogulMapApplicationTests.java
@@ -2,12 +2,18 @@ package com.neogulss.neogulmap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+/**
+ * Spring Boot 애플리케이션 컨텍스트 테스트
+ * 테스트 시 DB 연결 문제 회피를 위해 H2 사용 가능
+ */
 @SpringBootTest
+@ActiveProfiles("test") // test profile 사용 시 H2 DB 연결
 class NeogulMapApplicationTests {
 
   @Test
   void contextLoads() {
+    // 애플리케이션 컨텍스트가 정상적으로 로딩되는지 확인
   }
-
 }
