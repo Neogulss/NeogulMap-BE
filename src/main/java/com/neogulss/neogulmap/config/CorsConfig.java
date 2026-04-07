c2b8cfcf0c5cb8bad1859753d8cabafe06b7fe23
hyunho <melbua@naver.com>
2026-04-08 01:18:29 +0900

    corsconfig파일 추가

---
+++ src/main/java/com/neogulss/neogulmap/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.neogulss.neogulmap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://13.209.5.156")
+            .allowedMethods("*")
+            .allowCredentials(true);
+      }
+    };
+  }
+}
